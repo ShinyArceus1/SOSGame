52bb0926c594864f89313a74f68e809205a1ac42
Richie Bendall <richiebendall@gmail.com>
2018-06-27 12:06:26 +1200

    Fixed fatal error

--- game.py
+++ game.py
@@ -1,11 +1,11 @@
 import sos
 import ros
 
-shineprint("a game")
+sos.shineprint("a game")
 ros.delay(2)
-shinyspace(2)
-shineprint("for young people")
+sos.shinyspace(2)
+sos.shineprint("for young people")
 ros.delay(2)
-shinyspace(2)
-shineprint("who like math")
+sos.shinyspace(2)
+sos.shineprint("who like math")
 
